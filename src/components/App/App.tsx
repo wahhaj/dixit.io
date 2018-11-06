1a8809fd0779b65095b0db27b4c810ea7e65c08b
Wahhaj Ali <wahhajj@gmail.com>
2018-11-06 13:08:34 -0500

    cleaned up skeleton structure

---
+++ src/components/App/App.tsx
@@ -0,0 +1,17 @@
+import './App.css';
+
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          Dixit
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
