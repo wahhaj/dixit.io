641fe9bdc409795b08f16958f6e6e9545f458a6f
Wahhaj Ali <wahhajj@gmail.com>
2018-11-07 00:18:01 -0500

    setup basic page

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,20 @@
+import { Link, Route, BrowserRouter as Router } from "react-router-dom";
 import React, { Component } from "react";
+
+import Start from '../Start'
+import logo from './logo.png'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">Dixit</header>
-      </div>
+      <Router>
+        <div className="h-full flex flex-col">
+          <nav className="flex items-center justify-center p-2 bg-black text-gold"><img src={logo} alt="Dixit" className="h-12"></img></nav>
+          <div className="flex-1 p-2 bg-yellow-light text-black">
+            <Route path="/" exact component={Start} />
+          </div>
+        </div>
+      </Router>
     )
   }
 }
---
+++ src/components/Start/Start.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Start extends Component {
+  render() {
+    return (
+      <div className="flex flex-col items-center">
+        <div>Join</div>
+      </div>
+    )
+  }
+}
+
+export default Start
