4207c2433852b82b2aa84bd484122feb006b8c4c
Wahhaj Ali <wahhajj@gmail.com>
2018-11-07 02:01:16 -0500

    formatting

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,15 +1,18 @@
-import { Link, Route, BrowserRouter as Router } from "react-router-dom";
-import React, { Component } from "react";
+import { Link, Route, BrowserRouter as Router } from "react-router-dom"
+import React, { Component } from "react"
 
-import Start from '../Start'
-import logo from './logo.png'
+import Button from "../Button"
+import Start from "../Start"
+import logo from "./logo.png"
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div className="h-full flex flex-col">
-          <nav className="flex items-center justify-center p-2 bg-black text-gold"><img src={logo} alt="Dixit" className="h-12"></img></nav>
+          <nav className="flex items-center justify-center p-2 bg-black text-gold">
+            <img src={logo} alt="Dixit" className="h-12" />
+          </nav>
           <div className="flex-1 p-2 bg-yellow-light text-black">
             <Route path="/" exact component={Start} />
           </div>
--- src/components/Start/Start.tsx
+++ src/components/Start/Start.tsx
@@ -1,9 +1,14 @@
-import React, { Component } from "react";
+import React, { Component } from "react"
+
+import Button from "../Button"
 
 class Start extends Component {
   render() {
     return (
       <div className="flex flex-col items-center">
+        <div>
+          <Button label="Hello World" />
+        </div>
         <div>Join</div>
       </div>
     )
