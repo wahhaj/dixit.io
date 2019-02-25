93587cef4f76cfa720b5903b61fbd6bac35a24d4
Wahhaj Ali <wahhajj@gmail.com>
2019-02-25 05:03:13 -0500

    create_rooms

---
+++ server/lib/dixit_server/game/room.ex
@@ -0,0 +1,20 @@
+defmodule DixitServer.Game.Room do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+
+  schema "rooms" do
+    field :code, :string
+    field :state, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(room, attrs) do
+    room
+    |> cast(attrs, [:code, :state])
+    |> validate_required([:code, :state])
+    |> unique_constraint(:code)
+  end
+end
--- server/test/dixit_server/game/game_test.exs
+++ server/test/dixit_server/game/game_test.exs
@@ -61,4 +61,65 @@ defmodule DixitServer.GameTest do
       assert %Ecto.Changeset{} = Game.change_card(card)
     end
   end
+
+  describe "rooms" do
+    alias DixitServer.Game.Room
+
+    @valid_attrs %{code: "some code", state: "some state"}
+    @update_attrs %{code: "some updated code", state: "some updated state"}
+    @invalid_attrs %{code: nil, state: nil}
+
+    def room_fixture(attrs \\ %{}) do
+      {:ok, room} =
+        attrs
+        |> Enum.into(@valid_attrs)
+        |> Game.create_room()
+
+      room
+    end
+
+    test "list_rooms/0 returns all rooms" do
+      room = room_fixture()
+      assert Game.list_rooms() == [room]
+    end
+
+    test "get_room!/1 returns the room with given id" do
+      room = room_fixture()
+      assert Game.get_room!(room.id) == room
+    end
+
+    test "create_room/1 with valid data creates a room" do
+      assert {:ok, %Room{} = room} = Game.create_room(@valid_attrs)
+      assert room.code == "some code"
+      assert room.state == "some state"
+    end
+
+    test "create_room/1 with invalid data returns error changeset" do
+      assert {:error, %Ecto.Changeset{}} = Game.create_room(@invalid_attrs)
+    end
+
+    test "update_room/2 with valid data updates the room" do
+      room = room_fixture()
+      assert {:ok, %Room{} = room} = Game.update_room(room, @update_attrs)
+      assert room.code == "some updated code"
+      assert room.state == "some updated state"
+    end
+
+    test "update_room/2 with invalid data returns error changeset" do
+      room = room_fixture()
+      assert {:error, %Ecto.Changeset{}} = Game.update_room(room, @invalid_attrs)
+      assert room == Game.get_room!(room.id)
+    end
+
+    test "delete_room/1 deletes the room" do
+      room = room_fixture()
+      assert {:ok, %Room{}} = Game.delete_room(room)
+      assert_raise Ecto.NoResultsError, fn -> Game.get_room!(room.id) end
+    end
+
+    test "change_room/1 returns a room changeset" do
+      room = room_fixture()
+      assert %Ecto.Changeset{} = Game.change_room(room)
+    end
+  end
 end
