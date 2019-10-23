f3a422387655703867637bdfb5b56087024d3af8
Wahhaj Ali <wahhaj@wahhajali.com>
2019-10-23 01:44:06 -0400

    Added TailwindCSS with PurgeCSS

---
+++ postcss.config.js
@@ -0,0 +1,17 @@
+const purgeCSS = require("@fullhuman/postcss-purgecss")({
+  content: [
+    "./src/**/*.tsx",
+    "./src/index.css",
+    "./public/index.html",
+  ],
+  // Include any special characters you"re using in this regular expression
+  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+})
+
+module.exports = {
+  plugins: [
+    require("tailwindcss"),
+    require("autoprefixer"),
+    ...process.env.NODE_ENV === "production" ? [purgeCSS] : []
+  ],
+}
