a6a18b49337fe3ba77fe703be79a7273d154b630
Wahhaj Ali <wahhajj@gmail.com>
2019-10-30 11:19:11 -0400

    Added play and vote moves

---
+++ src/game/moves.ts
@@ -0,0 +1,43 @@
+import { IG } from "./typings"
+import { INVALID_MOVE, IGameCtx } from "boardgame.io/core"
+
+const play = (G: IG, ctx: IGameCtx, playCard: number) => {
+  const player = G.players[+ctx.playerID]
+  const cardIndexInHand = player.hand.indexOf(playCard)
+
+  if (cardIndexInHand !== -1) {
+    player.hand.splice(cardIndexInHand, 1)
+
+    G.playedCards.push({
+      player: +ctx.playerID,
+      card: playCard,
+      votes: [],
+    })
+
+    if (ctx.playerID === ctx.currentPlayer) {
+      ctx.events.setActivePlayers({
+        others: "play",
+        moveLimit: ctx.numPlayers === 3 ? 2 : 1,
+        next: {
+          others: "vote",
+          moveLimit: 1,
+        },
+      })
+    }
+  } else {
+    return INVALID_MOVE
+  }
+}
+
+const vote = (G: IG, ctx: IGameCtx, voteForCard: number) => {
+  const playedCard = G.playedCards.find(({ card }) => card === voteForCard)
+
+  if (playedCard && playedCard.player !== +ctx.playerID) {
+    playedCard.votes.push(+ctx.playerID)
+    G.numVotes++
+  } else {
+    return INVALID_MOVE
+  }
+}
+
+export default { play, vote }
