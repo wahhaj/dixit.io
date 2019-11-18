ebd1e02a609fb9eb5479d2d295afdf021a9c331e
Wahhaj Ali <wahhaj@wahhajali.com>
2019-11-18 01:15:37 -0500

    Added CardResizer component that calculates width+height for Cards to fit them into their container

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,17 +1,29 @@
 import React from "react"
-import styles from "./Card.module.css"
 import { CARD_IMAGE_URL_PREFIX } from "config"
 import back from "./back.png"
 
 type CardProps = {
   id?: number
   className?: string
+  width?: number
+  height?: number
 }
 
-const Card: React.FC<CardProps> = ({ id, className }) => {
+const Card: React.FC<CardProps> = ({ id, className, width = 180, height = 270 }) => {
   const url = id !== undefined ? `${CARD_IMAGE_URL_PREFIX}${id.toString().padStart(4, "0")}.png` : back
 
-  return <img src={url} alt={`Dixit card ${id}`} className={`${styles.Card} ${className} rounded-lg shadow`}></img>
+  return (
+    <img
+      src={url}
+      alt={`Dixit card ${id}`}
+      style={{
+        width: `${width}px`,
+        height: `${height}px`,
+        objectFit: "cover",
+      }}
+      className={`${className} rounded-lg shadow`}
+    ></img>
+  )
 }
 
 export default Card
