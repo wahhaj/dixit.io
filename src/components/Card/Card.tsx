4aebea3c27ed23da46e6d1a491625d8319a62bdd
Wahhaj Ali <wahhaj@wahhajali.com>
2019-11-19 05:24:40 -0500

    Allow Card to accept onClick handler

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -7,9 +7,10 @@ type CardProps = {
   className?: string
   width?: number
   height?: number
+  onClick?: () => void
 }
 
-const Card: React.FC<CardProps> = ({ id, className, width = 180, height = 270 }) => {
+const Card: React.FC<CardProps> = ({ id, className, width = 180, height = 270, onClick }) => {
   const url = id !== undefined ? `${CARD_IMAGE_URL_PREFIX}${id.toString().padStart(4, "0")}.png` : back
 
   return (
@@ -22,6 +23,7 @@ const Card: React.FC<CardProps> = ({ id, className, width = 180, height = 270 })
         objectFit: "cover",
       }}
       className={`${className} rounded-lg shadow`}
+      onClick={onClick}
     ></img>
   )
 }
