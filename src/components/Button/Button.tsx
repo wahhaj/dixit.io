016bad31f746656e07464157e8b5d41f8b559db9
Wahhaj Ali <wahhaj@wahhajali.com>
2019-11-26 15:48:16 -0500

    Button can accept style attribute

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,6 +4,7 @@ type ButtonProps = {
   disabled?: boolean
   type?: "button" | "submit"
   className?: string
+  style?: React.CSSProperties
   onClick?: () => void
 }
 
@@ -16,6 +17,7 @@ const Button: React.FC<ButtonProps> = (props) => {
       className={`flex items-center py-1 px-2 rounded border border-transparent focus:outline-none ${props.className} ${
         props.disabled ? "opacity-50 cursor-not-allowed" : "hover:shadow"
       }`}
+      style={props.style}
     >
       {props.children}
     </button>
