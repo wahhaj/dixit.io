4b9ee059fbf3aef1a127707b590b68238f63bbc5
Wahhaj Ali <wahhaj@wahhajali.com>
2019-11-30 07:30:53 -0500

    Added navbar with logo to Home

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -6,6 +6,12 @@ import logo from "logo.png"
 const Home: React.FC = (props) => {
   return (
     <div>
+      <nav className="bg-dark p-2 mb-4 text-center">
+        <img src={logo} alt="Dixit" className="h-8 md:h-12 md:mx-auto" />
+      </nav>
+      <div className="text-center">
+        <h1 className="text-2xl">Welcome to Dixit.io!</h1>
+      </div>
     </div>
   )
 }
