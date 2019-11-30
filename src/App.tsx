f0d64a9e3785bacf623956c9887033c41e00cf5e
Wahhaj Ali <wahhaj@wahhajali.com>
2019-11-30 07:30:27 -0500

    Added empty Home component at the base url

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,21 @@ import React from "react"
 import { Client } from "boardgame.io/react"
 import { SocketIO } from "boardgame.io/multiplayer"
 import { Dixit } from "game"
+import Home from "components/Home"
 import Board from "components/Board"
+import { BrowserRouter, Switch, Route } from "react-router-dom"
 
 const Game = Client({ game: Dixit, numPlayers: 3, board: Board, multiplayer: SocketIO({ server: "localhost:8000" }) })
 
-const App: React.FC = () => {
-  return <Game playerID={window.location.pathname.replace("/", "")} gameID="0" debug={false} />
-}
+// const App: React.FC = () => {
+//   return <Game playerID={window.location.pathname.replace("/", "")} gameID="0" debug={true} />
+// }
 
+const App: React.FC = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact={true} path={"/"} component={Home} />
+    </Switch>
+  </BrowserRouter>
+)
 export default App
---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import Button from "components/Button"
+import { IconPlayed, IconHand, IconScores } from "components/Icon"
+import logo from "logo.png"
+
+const Home: React.FC = (props) => {
+  return (
+    <div>
+    </div>
+  )
+}
+
+export default Home
