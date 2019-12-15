3570941fffe5e6126ab091bf8de9815600ed65aa
Wahhaj Ali <wahhaj@wahhajali.com>
2019-12-14 20:51:28 -0500

    Use Heroku port for server when available

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,6 @@ import { Server } from "boardgame.io/server"
 import { Dixit } from "./game"
 
 const server = Server({ games: [Dixit] })
-server.run(8000)
+const port = process.env.PORT || 8000
+
+server.run(port)
