e62ecdf1fa1231f55e729d6437a31904f37863ec
Wahhaj Ali <wahhaj@wahhajali.com>
2019-12-14 21:43:17 -0500

    Whitelisted some Tailwind classes that PurgeCSS was erroneously deleting

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,7 @@ const purgeCSS = require("@fullhuman/postcss-purgecss")({
   content: ["./src/**/*.tsx", "./src/components/**/*.css", "./src/index.css", "./public/index.html"],
   // Include any special characters you"re using in this regular expression
   defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+  whitelistPatterns: [/^text-\w+-500/, /^border-\w+-500/, /^bg-\w+-400/, /^bg-\w+-100/],
 })
 
 module.exports = {
